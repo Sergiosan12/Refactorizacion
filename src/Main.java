ffc82f0b20a174da63e5c547db416b37383ab257
Sergio <ssanromangiraldez@danielcastelao.org>
2024-04-15 09:48:57 +0200

    Renombrar variables

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,11 @@ public class Main {
     /**
      * metodo para devolver la puntuacion de tenis
      */
-    public static String getScore(int m_score1, int m_score2) {
+    public static String getScore(int pointsPlayer1, int pointsPlayer2) {
         String score = "";
-        int tempScore=0;
 
-        if (m_score1 == m_score2) {
-            switch (m_score1)
+        if (pointsPlayer1 == pointsPlayer2) {
+            switch (pointsPlayer1)
             {
                 case 0:
                     score = "Love-All";
@@ -27,21 +26,23 @@ public class Main {
 
             }
         }
-        else if (m_score1 >=4 || m_score2 >=4)
+        else if (pointsPlayer1 >=4 || pointsPlayer2 >=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
+            int pointsDiff = pointsPlayer1-pointsPlayer2;
+            if (pointsDiff==1) score ="Advantage player1";
+            else if (pointsDiff ==-1) score ="Advantage player2";
+            else if (pointsDiff>=2) score = "Win for player1";
             else score ="Win for player2";
         }
         else
         {
+
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
+                int auxScore=0;
+                if (i==1) auxScore = pointsPlayer1;
+                else { score+="-"; auxScore = pointsPlayer2;}
+                switch(auxScore)
                 {
                     case 0:
                         score+="Love";
