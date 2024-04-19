12c4de4d6aee51e6ac70ae3f1f7b45d8418f631f
Sergio <ssanromangiraldez@danielcastelao.org>
2024-04-19 10:05:50 +0200

    Refactorizados los métodos

--- src/com/dam/refactor/Main.java
+++ src/com/dam/refactor/Main.java
@@ -1,79 +1,82 @@
 package com.dam.refactor;
 
 public class Main {
+
     /**
      * @param pointsPlayer1 points of player 1
      * @param pointsPlayer2 points of player 2
      * @return the score of the game
      */
     public static String getScore(int pointsPlayer1, int pointsPlayer2) {
-        String score = "";
-        final int MAX_SCORE=4;
+        final int MAX_SCORE = 4;
 
-        /*
-        If the points of player 1 and player 2 are equal
-        */
         if (pointsPlayer1 == pointsPlayer2) {
-            switch (pointsPlayer1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
+            return getScoreWhenEqual(pointsPlayer1);
+        } else if (pointsPlayer1 >= MAX_SCORE || pointsPlayer2 >= MAX_SCORE) {
+            return getScoreWhenMoreThanMax(pointsPlayer1, pointsPlayer2);
+        } else {
+            return getScoreWhenLessThanMax(pointsPlayer1) + "-" + getScoreWhenLessThanMax(pointsPlayer2);
         }
-        /*
-        If the points of player 1 and player 2 are more or equal to 4
-        */
-        else if (pointsPlayer1 >=MAX_SCORE || pointsPlayer2 >=MAX_SCORE)
-        {
-            int pointsDiff = pointsPlayer1-pointsPlayer2;
-            if (pointsDiff==1) score ="Advantage player1";
-            else if (pointsDiff ==-1) score ="Advantage player2";
-            else if (pointsDiff>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        /*
-        If the points of player 1 and player 2 are less than 4 and different
-        */
-        else
-        {
-
-            for (int i=1; i<3; i++)
-            {
-                int auxScore=0;
-                if (i==1) auxScore = pointsPlayer1;
-                else { score+="-"; auxScore = pointsPlayer2;}
-                switch(auxScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-
-        }
-    return score;
     }
+
+    /**
+     *
+     * @param pointsPlayer
+     * @return the score of the game if the score is equal
+     */
+    //TODO: Refactor this method to use an Array instead of a switch
+    private static String getScoreWhenEqual(int pointsPlayer){
+            switch (pointsPlayer) {
+                case 0:
+                    return "Love-All";
+                case 1:
+                    return "Fifteen-All";
+                case 2:
+                    return "Thirty-All";
+                case 3:
+                    return  "Forty-All";
+                default:
+                    return "Deuce";
+
+        }
+    }
+
+    /**
+     *
+     * @param pointsPlayer1
+     * @param pointsPlayer2
+     * @return the score of the game if the score of both players is more than 4
+     */
+    private static String getScoreWhenMoreThanMax(int pointsPlayer1, int pointsPlayer2) {
+        int pointsDiff = pointsPlayer1 - pointsPlayer2;
+        if (pointsDiff == 1) {
+            return "Advantage player1";
+        } else if (pointsDiff == -1) {
+            return "Advantage player2";
+        } else if (pointsDiff >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
+        }
+    }
+
+
+    /**
+     *
+     * @param pointsPlayer
+     * @return the score of the player if the score is less than 4
+     */
+    private static String getScoreWhenLessThanMax(int pointsPlayer) {
+            switch (pointsPlayer) {
+                case 0:
+                    return "Love";
+                case 1:
+                    return "Fifteen";
+                case 2:
+                    return "Thirty";
+                default:
+                    return "Forty";
+            }
+        }
+
 }
